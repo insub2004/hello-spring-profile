8318e0895f1c63956615d14317db700674aef56b
insub2004 <insub2004@naver.com>
2024-04-22 00:31:08 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/hello/hellospringprofile/HelloSpringProfileApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.hello.hellospringprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringProfileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/hello/hellospringprofile/HelloSpringProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.hello.hellospringprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
